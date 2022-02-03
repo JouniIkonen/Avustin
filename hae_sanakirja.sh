a61d5b4370f15762d12c06437efc8c27d07b700a
Jouni Ikonen <jikonen@gmail.com>
2022-02-03 16:16:42 +0200

    lisätty tarkastus onko sanakirjatiedosto jo olemassa

--- hae_sanakirja.sh
+++ hae_sanakirja.sh
@@ -2,12 +2,19 @@
 
 
 # hakee sanalistan sivulta: https://kaino.kotus.fi/sanat/nykysuomi/
-wget https://kaino.kotus.fi/sanat/nykysuomi/kotus-sanalista-v1.tar.gz
-tar xfz kotus-sanalista-v1.tar.gz
+if [ ! -f sanat.txt ]; then
 
-# grep poistaa <s>  </s> tagien ulkopuoleiset osat xml tiedostosta
-# sed lauseke poistaa itse <s> ja </s> tagit  
-# syntyvä sanalista ohjeataan tiedostoon sanat.txt
-grep -o '<s>.*</s>' kotus-sanalista_v1/kotus-sanalista_v1.xml | sed -e 's/\(<s>\|<\/s>\)//g' > sanat.txt
+	wget https://kaino.kotus.fi/sanat/nykysuomi/kotus-sanalista-v1.tar.gz
+	tar xfz kotus-sanalista-v1.tar.gz
 
+	# grep poistaa <s>  </s> tagien ulkopuoleiset osat xml tiedostosta
+	# sed lauseke poistaa itse <s> ja </s> tagit  
+	# syntyvä sanalista ohjeataan tiedostoon sanat.txt
+	grep -o '<s>.*</s>' kotus-sanalista_v1/kotus-sanalista_v1.xml | sed -e 's/\(<s>\|<\/s>\)//g' > sanat.txt
+
+	echo "Sanakirjatiedosto haettu ja muunneettu sanat.txt tiedostoksi"
+
+else
+	echo "Sanakirjatiedosto sanat.txt on jo olemassa"
+fi
 
